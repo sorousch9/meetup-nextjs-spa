97fea9ba162674318b9752883db35fe0998b217e
Soroush Safarkhah <78909837+sorousch9@users.noreply.github.com>
2022-12-20 21:21:24 +0100

    app and mangodb connected

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+const dotenv = require("dotenv");
+dotenv.config();
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(process.env.MONGO_URL);
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
